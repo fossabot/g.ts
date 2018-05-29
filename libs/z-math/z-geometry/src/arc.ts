3405f4dd456aacd345d44bb84e25daa293425d16
LinBo Len <linbolen@gradii.com>
2018-05-29 21:43:45 +0800

    [WIP] rename file

--- libs/z-math/z-geometry/src/arc.ts
+++ libs/z-math/z-geometry/src/arc.ts
@@ -1,143 +1,149 @@
-const Util = require('../../util/index');
-const vec2 = require('../../util/matrix').vec2;
+/**
+ * @licence
+ * Copyright (c) 2018 LinBo Len <linbolen@gradii.com>
+ *
+ * Use of this source code is governed by an MIT-style license.
+ * See LICENSE file in the project root for full license information.
+ */
 
-function circlePoint(cx, cy, r, angle) {
-  return {
-    x: Math.cos(angle) * r + cx,
-    y: Math.sin(angle) * r + cy
-  };
-}
+import {mod} from '@gradii/g/core';
+import {vec2} from '@gradii/g/gl-matrix';
 
-function angleNearTo(angle, min, max, out) {
-  let v1;
-  let v2;
-  if (out) {
-    if (angle < min) {
-      v1 = min - angle;
-      v2 = Math.PI * 2 - max + angle;
-    } else if (angle > max) {
-      v1 = Math.PI * 2 - angle + min;
-      v2 = angle - max;
+export namespace arc {
+
+  function circlePoint(cx, cy, r, angle) {
+    return {
+      x: Math.cos(angle) * r + cx,
+      y: Math.sin(angle) * r + cy,
+    };
+  }
+
+  function angleNearTo(angle, min, max, out?) {
+    let v1;
+    let v2;
+    if (out) {
+      if (angle < min) {
+        v1 = min - angle;
+        v2 = Math.PI * 2 - max + angle;
+      } else if (angle > max) {
+        v1 = Math.PI * 2 - angle + min;
+        v2 = angle - max;
+      }
+    } else {
+      v1 = angle - min;
+      v2 = max - angle;
     }
-  } else {
-    v1 = angle - min;
-    v2 = max - angle;
+
+    return v1 > v2 ? max : min;
   }
 
-  return v1 > v2 ? max : min;
-}
-
-function nearAngle(angle, startAngle, endAngle, clockwise) {
-  let plus = 0;
-  if (endAngle - startAngle >= Math.PI * 2) {
-    plus = Math.PI * 2;
-  }
-  startAngle = Util.mod(startAngle, Math.PI * 2);
-  endAngle = Util.mod(endAngle, Math.PI * 2) + plus;
-  angle = Util.mod(angle, Math.PI * 2);
-  if (clockwise) {
-    if (startAngle >= endAngle) {
-      if (angle > endAngle && angle < startAngle) {
+  export function nearAngle(angle, startAngle, endAngle, clockwise) {
+    let plus = 0;
+    if (endAngle - startAngle >= Math.PI * 2) {
+      plus = Math.PI * 2;
+    }
+    startAngle = mod(startAngle, Math.PI * 2);
+    endAngle   = mod(endAngle, Math.PI * 2) + plus;
+    angle      = mod(angle, Math.PI * 2);
+    if (clockwise) {
+      if (startAngle >= endAngle) {
+        if (angle > endAngle && angle < startAngle) {
+          return angle;
+        }
+        return angleNearTo(angle, endAngle, startAngle, true);
+      }
+      if (angle < startAngle || angle > endAngle) {
         return angle;
       }
-      return angleNearTo(angle, endAngle, startAngle, true);
+      return angleNearTo(angle, startAngle, endAngle);
     }
-    if (angle < startAngle || angle > endAngle) {
+    if (startAngle <= endAngle) {
+      if (startAngle < angle && angle < endAngle) {
+        return angle;
+      }
+      return angleNearTo(angle, startAngle, endAngle, true);
+    }
+    if (angle > startAngle || angle < endAngle) {
       return angle;
     }
-    return angleNearTo(angle, startAngle, endAngle);
-  }
-  if (startAngle <= endAngle) {
-    if (startAngle < angle && angle < endAngle) {
-      return angle;
-    }
-    return angleNearTo(angle, startAngle, endAngle, true);
-  }
-  if (angle > startAngle || angle < endAngle) {
-    return angle;
-  }
-  return angleNearTo(angle, endAngle, startAngle);
-}
-
-function arcProjectPoint(cx, cy, r, startAngle, endAngle, clockwise, x, y, out) {
-  const v = [ x, y ];
-  const v0 = [ cx, cy ];
-  const v1 = [ 1, 0 ];
-  const subv = vec2.subtract([], v, v0);
-  let angle = vec2.angleTo(v1, subv);
-
-  angle = nearAngle(angle, startAngle, endAngle, clockwise);
-  const vpoint = [ r * Math.cos(angle) + cx, r * Math.sin(angle) + cy ];
-  if (out) {
-    out.x = vpoint[0];
-    out.y = vpoint[1];
-  }
-  const d = vec2.distance(vpoint, v);
-  return d;
-}
-
-function arcBox(cx, cy, r, startAngle, endAngle, clockwise) {
-  const angleRight = 0;
-  const angleBottom = Math.PI / 2;
-  const angleLeft = Math.PI;
-  const angleTop = Math.PI * 3 / 2;
-  const points = [];
-  let angle = nearAngle(angleRight, startAngle, endAngle, clockwise);
-  if (angle === angleRight) {
-    points.push(circlePoint(cx, cy, r, angleRight));
+    return angleNearTo(angle, endAngle, startAngle);
   }
 
-  angle = nearAngle(angleBottom, startAngle, endAngle, clockwise);
-  if (angle === angleBottom) {
-    points.push(circlePoint(cx, cy, r, angleBottom));
-  }
-
-  angle = nearAngle(angleLeft, startAngle, endAngle, clockwise);
-  if (angle === angleLeft) {
-    points.push(circlePoint(cx, cy, r, angleLeft));
-  }
-
-  angle = nearAngle(angleTop, startAngle, endAngle, clockwise);
-  if (angle === angleTop) {
-    points.push(circlePoint(cx, cy, r, angleTop));
-  }
-
-  points.push(circlePoint(cx, cy, r, startAngle));
-  points.push(circlePoint(cx, cy, r, endAngle));
-  let minX = Infinity;
-  let maxX = -Infinity;
-  let minY = Infinity;
-  let maxY = -Infinity;
-  Util.each(points, function(point) {
-    if (minX > point.x) {
-      minX = point.x;
-    }
-    if (maxX < point.x) {
-      maxX = point.x;
-    }
-    if (minY > point.y) {
-      minY = point.y;
-    }
-    if (maxY < point.y) {
-      maxY = point.y;
-    }
-  });
-
-  return {
-    minX,
-    minY,
-    maxX,
-    maxY
-  };
-}
-
-module.exports = {
-  nearAngle,
-  projectPoint(cx, cy, r, startAngle, endAngle, clockwise, x, y) {
+  export function projectPoint(cx, cy, r, startAngle, endAngle, clockwise, x, y) {
     const rst = {};
-    arcProjectPoint(cx, cy, r, startAngle, endAngle, clockwise, x, y, rst);
+    pointDistance(cx, cy, r, startAngle, endAngle, clockwise, x, y, rst);
     return rst;
-  },
-  pointDistance: arcProjectPoint,
-  box: arcBox
-};
+  }
+
+  export function pointDistance(cx, cy, r, startAngle, endAngle, clockwise, x, y, out) {
+    const v    = [x, y];
+    const v0   = [cx, cy];
+    const v1   = [1, 0];
+    const subv = vec2.subtract([], v, v0);
+    let angle  = vec2.angleTo(v1, subv);
+
+    angle        = nearAngle(angle, startAngle, endAngle, clockwise);
+    const vpoint = [r * Math.cos(angle) + cx, r * Math.sin(angle) + cy];
+    if (out) {
+      out.x = vpoint[0];
+      out.y = vpoint[1];
+    }
+    const d = vec2.distance(vpoint, v);
+    return d;
+  }
+
+  export function box(cx, cy, r, startAngle, endAngle, clockwise) {
+    const angleRight  = 0;
+    const angleBottom = Math.PI / 2;
+    const angleLeft   = Math.PI;
+    const angleTop    = Math.PI * 3 / 2;
+    const points      = [];
+    let angle         = nearAngle(angleRight, startAngle, endAngle, clockwise);
+    if (angle === angleRight) {
+      points.push(circlePoint(cx, cy, r, angleRight));
+    }
+
+    angle = nearAngle(angleBottom, startAngle, endAngle, clockwise);
+    if (angle === angleBottom) {
+      points.push(circlePoint(cx, cy, r, angleBottom));
+    }
+
+    angle = nearAngle(angleLeft, startAngle, endAngle, clockwise);
+    if (angle === angleLeft) {
+      points.push(circlePoint(cx, cy, r, angleLeft));
+    }
+
+    angle = nearAngle(angleTop, startAngle, endAngle, clockwise);
+    if (angle === angleTop) {
+      points.push(circlePoint(cx, cy, r, angleTop));
+    }
+
+    points.push(circlePoint(cx, cy, r, startAngle));
+    points.push(circlePoint(cx, cy, r, endAngle));
+    let minX = Infinity;
+    let maxX = -Infinity;
+    let minY = Infinity;
+    let maxY = -Infinity;
+    points.forEach((point) => {
+      if (minX > point.x) {
+        minX = point.x;
+      }
+      if (maxX < point.x) {
+        maxX = point.x;
+      }
+      if (minY > point.y) {
+        minY = point.y;
+      }
+      if (maxY < point.y) {
+        maxY = point.y;
+      }
+    });
+
+    return {
+      minX,
+      minY,
+      maxX,
+      maxY,
+    };
+  }
+}
--- libs/z-math/z-geometry/src/ellipse.ts
+++ libs/z-math/z-geometry/src/ellipse.ts
@@ -1,14 +1,25 @@
-module.exports = {
-  xAt(psi, rx, ry, cx, t) {
+/**
+ * @licence
+ * Copyright (c) 2018 LinBo Len <linbolen@gradii.com>
+ *
+ * Use of this source code is governed by an MIT-style license.
+ * See LICENSE file in the project root for full license information.
+ */
+
+export namespace ellipse {
+  export function xAt(psi, rx, ry, cx, t) {
     return rx * Math.cos(psi) * Math.cos(t) - ry * Math.sin(psi) * Math.sin(t) + cx;
-  },
-  yAt(psi, rx, ry, cy, t) {
+  }
+
+  export function yAt(psi, rx, ry, cy, t) {
     return rx * Math.sin(psi) * Math.cos(t) + ry * Math.cos(psi) * Math.sin(t) + cy;
-  },
-  xExtrema(psi, rx, ry) {
+  }
+
+  export function xExtrema(psi, rx, ry) {
     return Math.atan((-ry / rx) * Math.tan(psi));
-  },
-  yExtrema(psi, rx, ry) {
+  }
+
+  export function yExtrema(psi, rx, ry) {
     return Math.atan((ry / (rx * Math.tan(psi))));
   }
-};
+}
