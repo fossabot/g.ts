9733b80a542c0a76209537b7b568650c55071d55
LinBo Len <linbolen@gradii.com>
2018-05-31 11:29:00 +0800

    [WIP] working on z-math

---
+++ libs/z-math/z-matrix/src/aabb2.ts
@@ -0,0 +1,9 @@
+import {Vector2} from "./vector2";
+
+export class Aabb2 {
+
+  private min: Vector2;
+  private max: Vector2;
+
+
+}
---
+++ libs/z-math/z-matrix/src/aabb3.ts
@@ -0,0 +1,10 @@
+import {Vector3} from "./vector3";
+
+
+export class Aabb3 {
+
+  private min: Vector3;
+  private max: Vector3;
+
+
+}
